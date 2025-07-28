5158cde537a655f19b2f090ac03725720de371ef
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2025-07-28 10:50:51 +0700

    feat(cli): define create token mint, create ata, mint token procedure

--- apps/cli/src/web3.ts
+++ apps/cli/src/web3.ts
@@ -1,10 +1,22 @@
 import {
 	Context,
+	createAssociatedTokenAccount,
+	createTokenMint,
+	getMetadataAccountForTokenMint,
+	getTokenAccountAccount,
+	getTokenMintAccount,
 	KeypairsSigner,
+	mintToken,
+	setMetadataForFungibleToken,
 } from '@local/solana-web3'
+import {
+	findAssociatedTokenAccountAddress,
+	findTokenMetadataAddress,
+} from '@local/spl-core'
 import {
 	Connection,
 	Keypair,
+	PublicKey,
 } from '@solana/web3.js'
 import fs from 'fs'
 
@@ -15,11 +27,89 @@ export function createLocalContext(
 	keypairFile: string = '~/.config/solana/id.json',
 	rpcUrl: string = 'https://api.devnet.solana.com/',
 ): Context {
-	const connection = new Connection(rpcUrl)
+	const connection = new Connection(rpcUrl, 'finalized')
 	const defaultKeypairStr = fs.readFileSync(keypairFile, { encoding: 'utf8' })
 	const defaultKeypairBuf = Buffer.from(JSON.parse(defaultKeypairStr))
 	const signer = new KeypairsSigner([
 		Keypair.fromSecretKey(defaultKeypairBuf)
 	])
 	return new Context(connection, signer)
+}
+
+
+export async function exeCreateAssociatedTokenAccount(
+	ctx: Context,
+	tokenMint: PublicKey,
+	owner: PublicKey,
+): Promise<void> {
+	const associatedTokenAccount = findAssociatedTokenAccountAddress(tokenMint, owner)
+	let associatedTokenAccountAccount = await getTokenAccountAccount(ctx, associatedTokenAccount)
+	if (associatedTokenAccountAccount === null) {
+		await createAssociatedTokenAccount(
+			ctx,
+			tokenMint,
+			owner,
+			undefined,
+		)
+		associatedTokenAccountAccount = await getTokenAccountAccount(ctx, associatedTokenAccount)
+	}
+	console.debug(`Associated Token Account ${associatedTokenAccount.toBase58()} data`)
+	console.debug(associatedTokenAccountAccount)
+}
+
+export async function exeCreateTokenMint(
+	ctx: Context,
+	tokenMintKeypair: Keypair,
+): Promise<void> {
+	const tokenMint = tokenMintKeypair.publicKey
+	let tokenMintAcccount = await getTokenMintAccount(ctx, tokenMint)
+	if (tokenMintAcccount === null) {
+		await createTokenMint(
+			ctx,
+			tokenMintKeypair,
+			6,
+			undefined
+		)
+		tokenMintAcccount = await getTokenMintAccount(ctx, tokenMint)
+	}
+	console.debug(`Token Mint ${tokenMint.toBase58()} data`)
+	console.debug(tokenMintAcccount)
+}
+
+export async function exeMintToken(
+	ctx: Context,
+	tokenMint: PublicKey,
+	receipient: PublicKey,
+	amount: bigint,
+): Promise<void> {
+	await mintToken(
+		ctx,
+		tokenMint,
+		receipient,
+		amount,
+		undefined,
+	)
+}
+
+export async function exeSetTokenMetadata(
+	ctx: Context,
+	tokenMint: PublicKey,
+	name: string,
+	symbol: string,
+	metadataUri: string,
+): Promise<void> {
+	await setMetadataForFungibleToken(
+		ctx,
+		tokenMint,
+		name,
+		symbol,
+		metadataUri,
+		ctx.defaultSigner,
+		ctx.defaultSigner,
+		undefined,
+	)
+	const tokenMetadata = findTokenMetadataAddress(tokenMint)
+	const tokenMetadataAccount = await getMetadataAccountForTokenMint(ctx, tokenMint)
+	console.debug(`Token Metadata ${tokenMetadata.toBase58()} data`)
+	console.debug(tokenMetadataAccount)
 }
