3c22320a779546e911f916dea0978a8e21dd8b1b
Tung Nguyen <2715595+lukaz17@users.noreply.github.com>
2025-07-28 09:45:22 +0700

    feat(cli): allow override default keypair

--- apps/cli/src/web3.ts
+++ apps/cli/src/web3.ts
@@ -12,10 +12,11 @@ import fs from 'fs'
  * Initialize a Context using solana CLI keypair.
  */
 export function createLocalContext(
+	keypairFile: string = '~/.config/solana/id.json',
 	rpcUrl: string = 'https://api.devnet.solana.com/',
 ): Context {
 	const connection = new Connection(rpcUrl)
-	const defaultKeypairStr = fs.readFileSync('~/.config/solana/id.json', { encoding: 'utf8' })
+	const defaultKeypairStr = fs.readFileSync(keypairFile, { encoding: 'utf8' })
 	const defaultKeypairBuf = Buffer.from(JSON.parse(defaultKeypairStr))
 	const signer = new KeypairsSigner([
 		Keypair.fromSecretKey(defaultKeypairBuf)
